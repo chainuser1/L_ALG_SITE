378b85c6651e75570f7730d8237add15cddcea7f
Jayson Ramos <lothlorien654@gmail.com>
2019-04-05 09:47:35 +0800

    Add Admin Dashboard

---
+++ database/migrations/2019_04_04_062802_create_exercises_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateExercisesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exercises', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('pdf_location'.150);
+            $table->string('flash_location',150);
+            $table->string('answer',150);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('exercises');
+    }
+}
---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<link rel="apple-touch-icon" sizes="76x76" href="{{ asset('img/apple-icon.png') }}">
+		<link rel="icon" type="image/png" href="{{ asset('img/favicon.png') }}">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+		@yield('title')
+		<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+		<!--     Fonts and icons     -->
+		<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+		<link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+		<!-- CSS here -->
+		<link rel="stylesheet" href="{{ asset('vendor/mdbootstrap/css/bootstrap.min.css') }}">
+		<link rel="stylesheet" href="{{ asset('vendor/mdbootstrap/css/mdb.min.css') }}">
+		<link rel="stylesheet" href="{{ asset('css/magnific-popup.css') }}">
+		<link rel="stylesheet" href="{{ asset('css/font-awesome.css') }}">
+		<link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css') }}">
+		<link rel="stylesheet" href="{{ asset('css/slicknav.min.css') }}">
+		<link rel="stylesheet" href="{{ asset('css/animate.min.css') }}">
+		<link rel="stylesheet" href="{{ asset('css/style.min.css') }}">
+		<link rel="stylesheet" href="{{ asset('vendor/mdbootstrap/css/style.min.css') }}">
+		
+	</head>
+	<body>
+	</body>
+</html>
