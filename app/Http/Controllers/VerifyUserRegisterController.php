23e752f14d3bfcc5ea66b81fefb0e37b7f79ebd4
Jayson Ramos <lothlorien654@gmail.com>
2019-04-10 17:03:47 +0800

    Add Vue

--- app/Http/Controllers/VerifyUserRegisterController.php
+++ app/Http/Controllers/VerifyUserRegisterController.php
@@ -14,13 +14,13 @@ class VerifyUserRegisterController extends Controller
     public function validateUsername(Request $req){
     	$user=User::where('username','=',$req->username)->get();
     	if($user->isEmpty()){
-    		return response()->json(['message'=>'Username is ready for pick up',
-    			'status'=> 'true', 'class'=>'green-text'],200);
-    	}
-    	else{
-    		return response()->json(['message'=>'Username is already used.',
-    			'status'=> 'false', 'class'=>'orange-text'],200);
-    	}
+            return response()->json([
+                'status'=> true],200);
+        }
+        else{
+            return response()->json([
+                'status'=> false],200);
+        }
     }
 
     public function validateEmail(Request $req){
